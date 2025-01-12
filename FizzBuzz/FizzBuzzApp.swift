fa4e240f971ec9fb531d1fa18fd7628f9d6cab1d
Alldayallan <mail@allanreid.co>
2025-01-12 12:00:45 -0800

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Allan Reid on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
