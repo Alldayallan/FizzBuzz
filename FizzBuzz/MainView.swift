ee24a9c3e504cf52abd8c8c516ea31f6ab882dee
Alldayallan <mail@allanreid.co>
2025-01-12 12:18:51 -0800

    getting started

---
+++ FizzBuzz/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  FizzBuzz
+//
+//  Created by Allan Reid on 1/12/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            Text("FizzBuzzView").tabItem {
+                Label("FizzBuzz", systemImage: "bubbles.and.sparkles")
+            }
+            
+            Text("CodeCreatorView").tabItem {
+                Label("Code", systemImage: "note.text")
+            }
+            
+            Text("AboutView").tabItem {
+                Label("About", systemImage: "info.bubble.fill")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
