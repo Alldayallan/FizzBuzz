89342e3fee99f719e2cc3b83294d028c812c200e
Alldayallan <mail@allanreid.co>
2025-02-18 03:36:12 -0800

    main view

--- FizzBuzz/MainView.swift
+++ FizzBuzz/MainView.swift
@@ -5,17 +5,36 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         TabView {
-            Text("FizzBuzzView").tabItem {
+            BackgroundText(text: "FizzBuzzView", color: .red)
+            
+                .tabItem {
                 Label("FizzBuzz", systemImage: "bubbles.and.sparkles")
             }
             
-            Text("CodeCreatorView").tabItem {
-                Label("Code", systemImage: "note.text")
+            BackgroundText(text: "CodeCreatorView", color: .green)
+                .tabItem {
+                    Label("Code",
+                          systemImage: "note.text")
             }
             
-            Text("AboutView").tabItem {
-                Label("About", systemImage: "info.bubble.fill")
+            BackgroundText(text: "AboutView", color: .blue)
+                .tabItem {
+                    Label("About",
+                          systemImage: "info.bubble.fill")
             }
+        }
+    }
+}
+
+struct BackgroundText: View {
+    let text: String
+    let color: Color
+    var body: some View {
+        ZStack {
+            color.opacity(0.3).ignoresSafeArea()
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
         }
     }
 }
